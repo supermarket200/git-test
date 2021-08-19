3949302d8abbb22c41fbc22c692b699a790d807a
ye <ye@qq.com>
2021-08-19 17:18:46 +0800

    master commit2

--- src/main/java/com/ye/GitTest.java
+++ src/main/java/com/ye/GitTest.java
@@ -2,6 +2,8 @@ package com.ye;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("master1");
+        System.out.println("master2");
         System.out.println("hot-fix");
     }
 }
